1153229194493e9582b97d4c59ec349153d2f1b6
StevenKampinga <Steven.Kampinga@Outlook.com>
2016-12-21 13:35:59 +0100

    using CrossPlatformInputManager

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    Rigidbody rigidBody;
+    private float forceMultiplier = 1000;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+
+        if (CrossPlatformInputManager.GetAxis("Horizontal") != 0f) {
+            rigidBody.AddForce(Vector3.forward * CrossPlatformInputManager.GetAxis("Horizontal") * forceMultiplier * Time.deltaTime);
+        }
+	}
+}
